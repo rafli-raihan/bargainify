1b212f27362133ffdf219ad578a358dc9ae44b79
rafli-raihan <mrafliraihan@proton.me>
2024-11-28 21:32:07 +0700

    hero section 1

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -14,15 +14,37 @@ import 'package:flutter/material.dart';
                   icon: const Icon(
                     Icons.search,
                     color: Colors.black)
-                    ),
+                ),
                 IconButton(
                   onPressed: () {}, 
                   icon: const Icon(
                     Icons.menu,
                     color: Colors.black)
-                    ),
+                ),
               ],
             ),
+            body: Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.fromLTRB(20, 10, 20, 10), // Kalo mw padding semua sisinya disamaian EdgeInset.all(jumlah nya)
+              child: Column (
+                children: const <Widget>[
+                  Text(
+                    'Selamat Datang!',
+                    style: TextStyle(
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    "Temukan barang yang anda inginkan dengan harga yang fantastis",
+                    style: TextStyle(
+                      fontSize: 14.0,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  // Kalo mw nambah widget lagi disini
+                ]),
+              ),
           );
       }
     }
