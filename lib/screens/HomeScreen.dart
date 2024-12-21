b76bcfef23a843af0ccee15735a94357e8664d9a
rafli-raihan <mrafliraihan@proton.me>
2024-12-21 16:04:54 +0700

    Hero Section and Main Screen prototype

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -1,33 +1,79 @@
 import 'package:bargainify/widgets/scrollables/ProductsGridView.dart';
 import 'package:flutter/material.dart';
 import 'package:bargainify/models/Product.dart';
+import 'dart:math';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
+  double getResponsiveHeroHeight (BuildContext context){
+    return ( MediaQuery.of(context).size.width < 1024 ) ? 250 : 400;
+  }
+
   @override
   Widget build(BuildContext context) {
+    final Random random = Random();
+    final int randomIndex = random.nextInt(productList.length); // Random index within the productList range
+    final Product heroProduct = productList[randomIndex];
 
     return Scaffold(
       body: ListView(
-        padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-        children: <Widget>[
-          const Text(
-            'Selamat Datang!',
-            style: TextStyle(
-              fontSize: 30.0,
-              fontWeight: FontWeight.bold,
+        children: [
+          Container(
+            clipBehavior: Clip.hardEdge,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(15)),
+            ),  
+            padding: EdgeInsets.all(8),
+            height: getResponsiveHeroHeight(context),
+            child: Stack(
+              fit: StackFit.expand,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.all(Radius.circular(15)),
+                  child: 
+                Image.asset(
+                  heroProduct.imageAsset,
+                  fit: BoxFit.cover,
+                ),
+                ),
+                // Overlay with text
+                Container(
+                  padding: const EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                    color: Colors.black.withOpacity(0.5),
+                  ),                  
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text(
+                        'Selamat Datang!',
+                        style: TextStyle(
+                          fontSize: 30.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        "Temukan barang yang anda inginkan dengan harga yang fantastis",
+                        style: TextStyle(
+                          fontSize: 14.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ),
-          const Text(
-            "Temukan barang yang anda inginkan dengan harga yang fantastis",
-            style: TextStyle(
-              fontSize: 14.0,
-            ),
-            textAlign: TextAlign.start,
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ProductsGrid(products: productList),
           ),
-          ProductsGrid(products: productList)
-          // Add more widgets here if needed
         ],
       ),
     );
--- lib/widgets/scrollables/ProductsGridView.dart
+++ lib/widgets/scrollables/ProductsGridView.dart
@@ -4,7 +4,6 @@ import 'package:bargainify/screens/%5Bid%5D/ProductScreen.dart';
 
 class ProductsGrid extends StatelessWidget {
   final List<Product> products;
-
   const ProductsGrid({super.key, required this.products});
 
   double getResponsivePadding(BuildContext context){
@@ -16,93 +15,93 @@ class ProductsGrid extends StatelessWidget {
     return Padding(
       padding: EdgeInsets.all(getResponsivePadding(context)),
       child:
-    GridView.builder(
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 250, // Maximum width for each grid item
-        mainAxisSpacing: 10,
-        crossAxisSpacing: 10,
-        childAspectRatio: 3 / 4,
-        mainAxisExtent: 300,
+      GridView.builder(
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 250, // Max width buat containernya
+          mainAxisSpacing: 10,
+          crossAxisSpacing: 10,
+          childAspectRatio: 3 / 4,
+          mainAxisExtent: 300,
+        ),
+        itemCount: products.length,
+        shrinkWrap: true,
+        physics: const NeverScrollableScrollPhysics(),
+        itemBuilder: (BuildContext context, int index) {
+          final product = products[index];
+          return GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const ProductScreen()),
+              );
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.1),
+                    blurRadius: 5,
+                    spreadRadius: 1,
+                    offset: Offset(0, 2),
+                  ),
+                ],
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ClipRRect(
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(8),
+                      topRight: Radius.circular(8),
+                    ),
+                    child: Image.asset(
+                      product.imageAsset,
+                      height: 200,
+                      width: double.infinity,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          product.name,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 12,
+                          ),
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        const SizedBox(height: 4),
+                        Text(
+                          product.price,
+                          style: const TextStyle(
+                            color: Colors.grey,
+                            fontSize: 12,
+                          ),
+                        ),
+                        const SizedBox(height: 4),
+                        Text(
+                          product.location,
+                          style: const TextStyle(
+                            color: Colors.blueGrey,
+                            fontSize: 10,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
       ),
-      itemCount: products.length,
-      shrinkWrap: true,
-      physics: const NeverScrollableScrollPhysics(),
-      itemBuilder: (BuildContext context, int index) {
-        final product = products[index];
-        return GestureDetector(
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const ProductScreen()),
-            );
-          },
-          child: Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(8),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.1),
-                  blurRadius: 5,
-                  spreadRadius: 1,
-                  offset: Offset(0, 2),
-                ),
-              ],
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                ClipRRect(
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(8),
-                    topRight: Radius.circular(8),
-                  ),
-                  child: Image.asset(
-                    product.imageAsset,
-                    height: 200,
-                    width: double.infinity, // Matches the container's width
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        product.name,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 12,
-                        ),
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      const SizedBox(height: 4),
-                      Text(
-                        product.price,
-                        style: const TextStyle(
-                          color: Colors.grey,
-                          fontSize: 12,
-                        ),
-                      ),
-                      const SizedBox(height: 4),
-                      Text(
-                        product.location,
-                        style: const TextStyle(
-                          color: Colors.blueGrey,
-                          fontSize: 10,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        );
-      },
-    ),
     );
   }
 }
